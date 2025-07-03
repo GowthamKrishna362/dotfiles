d13ab7a75c9cfad0ee988be53657b0cdf5761e53
Gowtham <gks0321@gmail.com>
2025-07-03 23:06:33 +0530

    Changes

--- .config/nvim/lua/gowtham/plugins/oil.lua
+++ .config/nvim/lua/gowtham/plugins/oil.lua
@@ -6,6 +6,15 @@ return {
         view_options = {
           show_hidden = true,
         },
+        lsp_file_methods = {
+          -- Enable or disable LSP file operations
+          enabled = true,
+          -- Time to wait for LSP file operations to complete before skipping
+          timeout_ms = 5000,
+          -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+          -- Set to "unmodified" to only save unmodified buffers
+          autosave_changes = true,
+        },
       })
       vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Toggle file explorer on current file" })
     end,
